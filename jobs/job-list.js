fd33c6de17a8503f180344037c278e782335ebfa
Illiyas124 <illiyasin2001@gmail.com>
2024-11-23 17:32:06 +0530

    fixed_features

---
+++ jobs/job-list.js
@@ -0,0 +1,24 @@
+const jCategory = [
+    {
+        index: 0,
+        image: "../images/google.png",
+        title: "Web Developer",
+        rate: '$900-1200/m',
+        av: 'Full Time',
+        companyName: 'Google',
+        location: 'USA',
+        vacancy: '01',
+        hours: '50h / week',
+        description: "We are looking for a skilled Web Developer to join our dynamic team. The ideal candidate will have experience in both front-end and back-end development, a passion for building user-friendly websites, and the ability to collaborate with cross-functional teams to deliver high-quality web solutions.",
+        workplace: 'Work from home',
+        education: "For a Web Developer position, a bachelor's degree in Computer Science, Information Technology, Web Development, or a related field is preferred. However, candidates with practical experience and a strong portfolio showcasing web development projects may also be considered, even without formal educational credentials. A solid understanding of web technologies, including HTML, CSS, JavaScript, and web frameworks, along with familiarity with back-end development and databases, is essential for success in this role. Continuous learning through online courses or certifications in web development is highly valued.",
+        experience: "2 to 5year(s)",
+    },
+    {
+        index: 1,
+        image: "../images/uber.png",
+        title: "Business Associate",
+        rate: '$900-1200/m',
+        av: 'Part Time',
+    }
+];
